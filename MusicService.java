d55b9618876331e7bc2cd5db471dbce6e56cf075
Caileannn <Caileannn@gmail.com>
2017-04-26 17:22:18 +0100

    Music Service Added

---
+++ MusicService.java
@@ -0,0 +1,37 @@
+package com.lastfmdatabase.loginregister.activities;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+
+import com.lastfmdatabase.loginregister.R;
+
+/**
+ * Created by Cailean on 18/04/2017.
+ */
+
+public class MusicService extends Service {
+    private  MediaPlayer player;
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player = MediaPlayer.create(this, R.raw.background_music);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        player.stop();
+    }
+}
